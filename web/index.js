491d03926753d0dabf0dd800bbcc6ba06eb1f15a
lirzetouny@gmail.com <lirzetouny@gmail.com>
2023-01-15 12:26:38 +0200

    first commit

---
+++ web/index.js
@@ -0,0 +1,10 @@
+import {
+  extend,
+
+} from "@shopify/checkout-ui-extensions";
+
+// Set the entry point for the extension
+extend("Checkout::Reductions::RenderBefore", (root, api) => {
+
+});
+z
